3a9ea4373f769e4cfeed7781125cba2dd9ce139e
unknown <avanikhede15@gmail.com>
2024-03-16 19:39:47 +0530

    registration

---
+++ jobportal/src/Component/Signup.jsx
@@ -0,0 +1,72 @@
+import React, { useState } from 'react';
+import "../CSS/Reg.css"
+import ForwardToInboxIcon from '@mui/icons-material/ForwardToInbox';
+
+function RegistrationForm() {
+  const [accountType, setAccountType] = useState('Personal');
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [gender, setGender] = useState('Male');
+
+  const handleAccountTypeChange = (event) => {
+    setAccountType(event.target.value);
+  };
+
+  const handleNameChange = (event) => {
+    setName(event.target.value);
+  };
+
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleGenderChange = (event) => {
+    setGender(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Handle form submission here
+  };
+
+  return (
+   <>
+   <div class="testbox">
+  <h1>Registration</h1>
+
+  <form action="/">
+      <hr/>
+    <div class="accounttype">
+      <input type="radio" value="None" id="radioOne" name="account" checked/>
+      <label for="radioOne" class="radio" chec>Personal</label>
+      <input type="radio" value="None" id="radioTwo" name="account" />
+      <label for="radioTwo" class="radio">Company</label>
+    </div>
+  <hr/>
+  <label id="icon" for="name"><i class="icon-envelope "></i></label>
+  <input type="text" name="name" id="name" placeholder="Email" required/>
+  <label id="icon" for="name"><i class="icon-user"></i></label>
+  <input type="text" name="name" id="name" placeholder="Name" required/>
+  <label id="icon" for="name"><i class="icon-shield"></i></label>
+  <input type="password" name="name" id="name" placeholder="Password" required/>
+  <div class="gender">
+    <input type="radio" value="None" id="male" name="gender" checked/>
+    <label for="male" class="radio" chec>Male</label>
+    <input type="radio" value="None" id="female" name="gender" />
+    <label for="female" class="radio">Female</label>
+   </div> 
+   <p>By clicking Register, you agree on our <a href="#">terms and condition</a>.</p>
+   <a href="#" class="button">Register</a>
+  </form>
+</div>
+   
+   </>
+  );
+}
+
+export default RegistrationForm;
