746322fb7982294bc1220303424709057a0b34fc
prattsss <prattushvyas@gmail.com>
2024-03-16 22:34:25 +0530

    new update

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import JobCards from "./Components/JobCards";
+import LandingPage from "./Components/LandingPage";
+import NavBar from "./Components/NavBar";
+
+function App() {
+  return (
+    <>
+      <LandingPage />
+      <NavBar />
+      <JobCards />
+    </>
+
+  )
+}
+export default App;
---
+++ src/Components/constant.js
@@ -0,0 +1,11 @@
+const data = [{
+  job :{
+    "profile" : "Receptioninst",
+    "compony" : "IAGA",
+    "location": "Navlakha,Indore,Madhya Pradesh",
+    "pay": "₹20,000 - ₹25,000 a month",
+    "job": ["Full-time","Fresher","Permanent"],
+    "shift": "Day shift",
+    
+  }
+}]
